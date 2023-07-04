bf7fc8284f90370e43b39cfe9a7cd721f4b0dd9f
Loick Adjiwanou <loickadjiwanou1111@gmail.com>
2023-07-04 18:30:32 +0100

    Commit

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const { genSalt, hash, compare } = require("bcrypt");
 const mongoose = require("mongoose");
 
+const jwt = require('jsonwebtoken');
+
+
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
@@ -14,12 +17,17 @@ mongoose.connect("mongodb://localhost:27020/api_token", {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Erreur de connection:"));
 db.once("open", () => {
-  console.log("Connection reussie !");
+  console.log("Connected to Mongo Database !");
 });
 
 // Définition modèle utilisateur
 const userSchema = new mongoose.Schema({
   username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
     type: String,
     required: true,
     unique: true,
@@ -55,11 +63,26 @@ const User = mongoose.model("User", userSchema);
 
 // Route pour créer un nouvel utilisateur
 app.post("/signup", async (req, res) => {
-  const { username, password } = req.body;
-  const user = new User({ username, password });
+  const { username, email, password } = req.body;
+
+  // Vérifier si le nom d'utilisateur existe déjà
+  const usernameExists = await User.findOne({ username });
+  if (usernameExists) {
+    return res.status(400).json({ error: "Veuillez choisir un autre nom d'utilisateur" });
+  }
+
+  const user = new User({ username, email, password });
   try {
     await user.save();
-    res.json({ message: "User created successfully" });
+
+    // Générer un token
+    const token = jwt.sign({ username: user.username }, 'mongoTOken');
+
+    //Ajout du token au header
+    res.header('x-auth-token', token);
+
+    // Renvoyer le token dans la réponse
+    res.json({ message: `User ${username} crééres.header('x-auth-token', token); !` });
   } catch (err) {
     console.error("Failed to create user:", err);
     res.status(500).json({ error: `Failed to create user ==> ${err.message}` });
@@ -69,7 +92,7 @@ app.post("/signup", async (req, res) => {
 // Route pour se connecter en tant qu'utilisateur
 
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   // User.findOne({ username }, (err, user) => {
   //   if (err) {
   //     console.error("Utilisateur intouvable:", err);
@@ -89,7 +112,7 @@ app.post("/login", async (req, res) => {
   //   });
   // });
   try {
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ email });
     if (!user) {
      return res.status(401).json({ error: "Accès invalides" });
     }
@@ -100,15 +123,18 @@ app.post("/login", async (req, res) => {
       return;
     }
   
-    res.json({ message: "Connection reussie" });
+    res.json({ message: "Connection reussie !", token: req.body.token});
   } catch (err) {
     console.error("Utilisateur intouvable:", err);
     res.status(500).json({ error: "Utilisateur intouvable" });
   }  
 });
 
+
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+
