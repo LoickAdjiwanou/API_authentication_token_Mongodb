0deff4d0ff68f4e741940cb549418d0018119a7e
Loick Adjiwanou <loickadjiwanou1111@gmail.com>
2023-07-04 16:39:49 +0100

    Commit

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const { genSalt, hash, compare } = require("bcrypt");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
-mongoose.connect('mongodb://localhost:27017/api_token', {
+mongoose.connect("mongodb://localhost:27020/api_token", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erreur de connection:'));
-db.once('open', () => {
-  console.log('Connection reussie !');
+db.on("error", console.error.bind(console, "Erreur de connection:"));
+db.once("open", () => {
+  console.log("Connection reussie !");
 });
 
 // Définition modèle utilisateur
@@ -30,62 +30,81 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', function (next) {
-  const user = this;
-  if (!user.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
     return next();
   }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) {
-        return next(err);
-      }
-      user.password = hash;
-      next();
-    });
-  });
+  const salt = await genSalt(10);
+  this.password = await hash(this.password, salt);
+  next();
+  // bcrypt.genSalt(10, (err, salt) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   bcrypt.hash(this.password, salt, (err, hash) => {
+  //     if (err) {
+  //       return next(err);
+  //     }
+  //     this.password = hash;
+  //     next();
+  //   });
+  // });
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // Route pour créer un nouvel utilisateur
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { username, password } = req.body;
   const user = new User({ username, password });
   try {
     await user.save();
-    res.json({ message: 'User created successfully' });
+    res.json({ message: "User created successfully" });
   } catch (err) {
-    console.error('Failed to create user:', err);
-    res.status(500).json({ error: 'Failed to create user' });
+    console.error("Failed to create user:", err);
+    res.status(500).json({ error: `Failed to create user ==> ${err.message}` });
   }
 });
 
-
 // Route pour se connecter en tant qu'utilisateur
-app.post('/login', (req, res) => {
+
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      console.error('Utilisateur intouvable:', err);
-      res.status(500).json({ error: 'Utilisateur intouvable' });
-      return;
-    }
+  // User.findOne({ username }, (err, user) => {
+  //   if (err) {
+  //     console.error("Utilisateur intouvable:", err);
+  //     res.status(500).json({ error: "Utilisateur intouvable" });
+  //     return;
+  //   }
+  //   if (!user) {
+  //     res.status(401).json({ error: "Accès invalides" });
+  //     return;
+  //   }
+  //   bcrypt.compare(password, user.password, (err, result) => {
+  //     if (err || !result) {
+  //       res.status(401).json({ error: "Accès invalides" });
+  //       return;
+  //     }
+  //     res.json({ message: "Connection reussie" });
+  //   });
+  // });
+  try {
+    const user = await User.findOne({ username });
     if (!user) {
-      res.status(401).json({ error: 'Accès invalides' });
+     return res.status(401).json({ error: "Accès invalides" });
+    }
+  
+    const result = await compare(password, user.password);
+    if (!result) {
+      res.status(401).json({ error: "Accès invalides" });
       return;
     }
-    bcrypt.compare(password, user.password, (err, result) => {
-      if (err || !result) {
-        res.status(401).json({ error: 'Accès invalides' });
-        return;
-      }
-      res.json({ message: 'Connection reussie' });
-    });
-  });
+  
+    res.json({ message: "Connection reussie" });
+  } catch (err) {
+    console.error("Utilisateur intouvable:", err);
+    res.status(500).json({ error: "Utilisateur intouvable" });
+  }  
 });
 
 const port = 3000;
